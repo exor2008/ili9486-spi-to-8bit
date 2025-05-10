53b90743992a44f8ef6d792cbd095598a8ee4715
Ian <exor2008@yandex.ru>
2025-05-10 16:51:25 -0400

    SPI slave complicated algorithm
    Switch to embassy master branch

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Level, Pull};
+use embassy_rp::clocks::ClockConfig;
+use embassy_rp::config::Config;
+use embassy_rp::gpio::{Level, Output, Pull};
+use embassy_rp::spi::{Config as SpiConfig, Phase, Polarity, Spi};
 use embassy_rp::{
     bind_interrupts,
     gpio::Input,
     peripherals::PIO0,
     pio::{InterruptHandler, Pio},
 };
+use embassy_time::Timer;
 use heapless::Vec;
 use ili9486display::spi_in::SpiIn;
 use {defmt_rtt as _, panic_probe as _};
@@ -22,7 +26,10 @@ bind_interrupts!(struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Started");
-    let p = embassy_rp::init(Default::default());
+    let config = ClockConfig::system_freq(200_000_000);
+    let cfg = Config::new(config);
+    let p = embassy_rp::init(cfg);
+    // let p = embassy_rp::init(Default::default());
 
     let Pio {
         mut common, sm0, ..
@@ -43,23 +50,39 @@ async fn main(_spawner: Spawner) {
 
     loop {
         spi_in.read(&mut data).await;
-        info!("Read: {:#x}", data);
+        let command = if (data[0] & 256) == 0 { "Command" } else { "" };
+        info!("Read: {:#x} {}", data[0] & 0xFF, command);
     }
 
-    // let clk = Input::new(p.PIN_12, Pull::Down);
-    // let cs = Input::new(p.PIN_13, Pull::Down);
+    // let miso = p.PIN_12;
+    // let mosi = p.PIN_11;
+    // let clk = p.PIN_10;
+    // let mut cs = Output::new(p.PIN_13, Level::High);
+    // let mut dc = Output::new(p.PIN_14, Level::Low);
 
-    // let mut acc = false;
-    // let mut prev_clocl = false;
+    // let mut cfg = SpiConfig::default();
+    // cfg.frequency = 16_000_000;
+
+    // let mut spi = Spi::new(p.SPI1, clk, mosi, miso, p.DMA_CH0, p.DMA_CH1, cfg);
 
     // loop {
-    //     if cs.get_level() == Level::Low {
-    //         acc = true;
-
-    //     } else if acc {
-    //         acc = false;
+    //     for i in 1..=5 {
+    //         cs.set_low();
+    //         // Timer::after_millis(100).await;
+    //         let tx_buf = [i];
+    //         let mut rx_buf = [0_u8; 1];
+    //         dc.set_low();
+    //         spi.transfer(&mut rx_buf, &tx_buf).await.unwrap();
+    //         info!("Command {}", i);
+    //         dc.set_high();
+    //         for j in 0..i {
+    //             let tx_buf = [j];
+    //             spi.transfer(&mut rx_buf, &tx_buf).await.unwrap();
+    //             info!("Data {}", j);
+    //         }
+    //         cs.set_high();
+    //         Timer::after_secs(1).await;
     //     }
+    //     Timer::after_secs(5).await;
     // }
-
-    // let s = Spi
 }
